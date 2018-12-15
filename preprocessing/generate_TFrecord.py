791f70a624d72242cf1e0e176699b1ed1ea1c9d1
Aayush Shrivastav <32818463+aayushs879@users.noreply.github.com>
2018-12-15 22:19:20 +0530

    Add files via upload

---
+++ preprocessing/generate_TFrecord.py
@@ -0,0 +1,115 @@
+"""
+Usage:
+  # From tensorflow/models/
+  # Create train data:
+  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
+  # Create test data:
+  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+"""
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+import sys
+import os
+import io
+import pandas as pd
+import tensorflow as tf
+import argparse
+
+sys.path.append('/home/aayushs879/tensorflow/models/research')
+
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+
+
+
+
+# TO-DO replace this with label map
+def class_text_to_int(row_label):
+    if row_label == 'RBC':
+        return 1
+    elif row_label == 'WBC':
+        return 2
+    elif row_label == 'Platelets':
+        return 3
+    else:
+        None
+
+
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+if __name__ == '__main__':
+
+    parse = argparse.ArgumentParser(description = 'csv to rec converter')
+
+    parse.add_argument('-i', '--input_dir', help = "Path to csv input directory")
+    parse.add_argument('-img', '--image_dir', help = "Path to image directory")
+    parse.add_argument('-o', '--output_path', help = "Path to output record file")
+    args = parse.parse_args()
+
+    writer = tf.python_io.TFRecordWriter(args.output_path)
+    path = os.path.join(args.image_dir)
+    a = args.input_dir
+    examples = pd.read_csv(a)
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+
+    writer.close()
+
+    print('Successfully created the TFRecords: {}'.format(args.output_path))
+
+
+
+   
+
+
+
